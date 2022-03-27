90ee2836760da46db000ee520a206d1e523a307b
Wilson Junior <wilsonraj@unipam.edu.br>
2022-03-27 15:41:16 -0300

    Iniciando criação do módulo de folders, faltando criar as rotas de update, delete e findOne

---
+++ src/controllers/folderController.js
@@ -0,0 +1,33 @@
+const FolderModel = require('../models/folderModel');
+
+async function createFolder(req, res) { 
+    const body = req.body
+    try {
+        const verificarFolder = await FolderModel.findOne({ folder: body.folder });
+        if (verificarFolder) return res.status(400).json({Erro: "Já existe uma pasta criada com ese nome!"});
+
+        const folder = await FolderModel.create(body);
+        return res.status(201).json({Sucesso: "Nova pasta criada com sucesso.", folder})
+
+    } catch (error) {
+        return res.status(400).json({Erro: "Requisição Inválida."});
+    }
+}
+
+async function findOne(req, res) { 
+    const { title } = req.params;
+
+    if(req.title != title) return res.status(400).json({Erro: "Pasta não encontrada."})
+    try {
+        const titleFolder = await FolderModel.findNameByTitle(title);
+        return res.status(200).json(titleFolder);
+
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+}
+
+module.exports = {
+    createFolder,
+    findOne
+}
