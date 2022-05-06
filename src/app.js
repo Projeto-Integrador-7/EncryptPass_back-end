62ae27827b1218529d9d8ba51a8de6badfc7f109
Brayan Frank <brayanfrank.sm@gmail.com>
2022-05-06 14:32:57 -0300

    some security corrections

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, 
-	max: 500, 
+	max: 5, 
 	standardHeaders: true, 
 	legacyHeaders: false, 
 })
